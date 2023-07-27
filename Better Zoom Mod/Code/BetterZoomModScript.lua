2c97c2e66d393dc959aea535f1e83f73eaa841b0
Christian Hoffmann <chris.hoffmann84@gmail.com>
2023-07-27 22:04:30 +0200

    fixed bugs

--- Better Zoom Mod/Code/BetterZoomModScript.lua
+++ Better Zoom Mod/Code/BetterZoomModScript.lua
@@ -3,7 +3,7 @@ local CurrentModDef = CurrentModDef
 local options = CurrentModDef.options
 
 local function ApplyMod()
-    print("Better Zoom Mod: ApplyMod() called")
+    -- print("Better Zoom Mod -> ApplyMod() called")
 
     options = CurrentModDef.options
     hr.CameraTacMaxZoom = tonumber(options.BetterZoomModCameraTacMaxZoom)
@@ -11,13 +11,46 @@ local function ApplyMod()
     -- hr.CameraTacMaxZoomOverview = tonumber(options.BetterZoomModCameraTacOverviewZoom)
     hr.CameraTacZoomStep = tonumber(options.BetterZoomModCameraTacZoomStep)
 
-    print("Set camera options to: ", options)
+    hr.AaaTest = "TEST"
+
+    print("Better Zoom Mod -> Set camera options to: ", options)
 end
 
 OnMsg.ApplyModOptions = ApplyMod
 OnMsg.ModsReloaded = ApplyMod
 
+OnMsg.TurnStart = function(team)
+    -- print("Better Zoom Mod -> TurnStart handler")
+
+    local team = g_Teams[team]
+    -- print("Turn STARTED -> team.player_team", team.player_team)
+
+    cameraTac.SetForceMaxZoom(false)
+
+    if team.player_team == true then
+        -- print("palyer's turn started")
+        hr.CameraTacMaxZoom = tonumber(options.BetterZoomModCameraTacMaxZoom)
+        print("Better Zoom Mod -> START OF PLAYER'S TURN: set max zoom to",
+              hr.CameraTacMaxZoom)
+    end
+end
+
+OnMsg.TurnEnded = function(team)
+    -- print("Better Zoom Mod -> TurnEnded handler")
+
+    local team = g_Teams[team]
+    -- print("Turn ENDED -> team.player_team", team.player_team)
+
+    if team.player_team == true then
+        -- print("palyer's turn ended")
+        hr.CameraTacMaxZoom = tonumber(options.BetterZoomModCameraTacMaxZoomAI)
+        cameraTac.SetForceMaxZoom(true)
+        print("Better Zoom Mod -> END OF PLAYER'S TURN: set max zoom to",
+              hr.CameraTacMaxZoom)
+    end
+end
+
 OnMsg.GameStarted = function()
-    print("Better Zoom Mod: received GameStarted")
+    print("Better Zoom Mod -> Received GameStarted")
     ApplySettings()
 end
--- Better Zoom Mod/items.lua
+++ Better Zoom Mod/items.lua
@@ -1,142 +1,84 @@
 return {
-PlaceObj('ModItemCode', {
-	'name', "BetterZoomModScript",
-	'CodeFileName', "Code/BetterZoomModScript.lua",
-}),
-PlaceObj('ModItemGameRuleDef', {
-	id = "1",
-	msg_reactions = {
-		PlaceObj('MsgReaction', {
-			Event = "TurnStart",
-			Handler = function (self, team)
-				local options = AccountStorage.ModOptions.BetterZoomMod
-				
-				local team = g_Teams[team]
-				-- print("Turn STARTED -> team.player_team", team.player_team)
-				
-				cameraTac.SetForceMaxZoom(false)
-				
-				if team.player_team == true then
-					-- print("palyer's turn started")
-					hr.CameraTacMaxZoom = tonumber(options.BetterZoomModCameraTacMaxZoom)
-					print("START OF PLAYER'S TURN: set max zoom to", hr.CameraTacMaxZoom)
-				end
-			end,
-			HandlerCode = function (self, team)
-				local options = AccountStorage.ModOptions.BetterZoomMod
-				
-				local team = g_Teams[team]
-				-- print("Turn STARTED -> team.player_team", team.player_team)
-				
-				cameraTac.SetForceMaxZoom(false)
-				
-				if team.player_team == true then
-					-- print("palyer's turn started")
-					hr.CameraTacMaxZoom = tonumber(options.BetterZoomModCameraTacMaxZoom)
-					print("START OF PLAYER'S TURN: set max zoom to", hr.CameraTacMaxZoom)
-				end
-			end,
-			param_bindings = false,
-		}),
-		PlaceObj('MsgReaction', {
-			Event = "TurnEnded",
-			Handler = function (self, teamEnded)
-				local options = AccountStorage.ModOptions.BetterZoomMod
-				
-				local team = g_Teams[teamEnded]
-				-- print("Turn ENDED -> team.player_team", team.player_team)
-				
-				if team.player_team == true then
-					-- print("palyer's turn ended")
-					hr.CameraTacMaxZoom = tonumber(options.BetterZoomModCameraTacMaxZoomAI)
-					cameraTac.SetForceMaxZoom(true)
-					print("END OF PLAYER'S TURN: set max zoom to", hr.CameraTacMaxZoom)
-				end
-			end,
-			HandlerCode = function (self, teamEnded)
-				local options = AccountStorage.ModOptions.BetterZoomMod
-				
-				local team = g_Teams[teamEnded]
-				-- print("Turn ENDED -> team.player_team", team.player_team)
-				
-				if team.player_team == true then
-					-- print("palyer's turn ended")
-					hr.CameraTacMaxZoom = tonumber(options.BetterZoomModCameraTacMaxZoomAI)
-					cameraTac.SetForceMaxZoom(true)
-					print("END OF PLAYER'S TURN: set max zoom to", hr.CameraTacMaxZoom)
-				end
-			end,
-			param_bindings = false,
-		}),
-	},
-}),
-PlaceObj('ModItemOptionChoice', {
-	'name', "BetterZoomModCameraTacMaxZoom",
-	'comment', "Vanilla Default = 130, Mod Default = 300",
-	'DisplayName', "Maximum Camera Zoom",
-	'Help', "Defines how far the camera may zoom out (Vanilla Default = 130)",
-	'DefaultValue', "300",
-	'ChoiceList', {
-		"130",
-		"200",
-		"300",
-		"400",
-		"500",
-		"600",
-		"700",
-		"800",
-	},
-}),
-PlaceObj('ModItemOptionChoice', {
-	'name', "BetterZoomModCameraTacMinZoom",
-	'comment', "Vanilla Default = 65, Mod Default = 70",
-	'DisplayName', "Minimum Camera Zoom",
-	'Help', "Defines how far the camera may zoom in (Vanilla Default = 65)",
-	'DefaultValue', "70",
-	'ChoiceList', {
-		"100",
-		"90",
-		"80",
-		"70",
-		"65",
-		"60",
-		"50",
-		"40",
-		"30",
-		"20",
-		"10",
-	},
-}),
-PlaceObj('ModItemOptionChoice', {
-	'name', "BetterZoomModCameraTacMaxZoomAI",
-	'comment', "Vanilla Default = 130, Mod Default = 130",
-	'DisplayName', "Maximum Camera Zoom During AI Turns",
-	'Help', "Defines the camera's zoom level during enemy turns (Vanilla Default = 130)",
-	'DefaultValue', "130",
-	'ChoiceList', {
-		"130",
-		"200",
-		"300",
-		"400",
-		"500",
-		"600",
-		"700",
-		"800",
-	},
-}),
-PlaceObj('ModItemOptionChoice', {
-	'name', "BetterZoomModCameraTacZoomStep",
-	'comment', "Vanilla Default = 25, Mod Default = 50",
-	'DisplayName', "Zoom Step Size",
-	'Help', "Defines how fast the camera zoom (Vanilla Default = 25)",
-	'DefaultValue', "50",
-	'ChoiceList', {
-		"10",
-		"25",
-		"50",
-		"75",
-		"100",
-		"125",
-	},
-}),
+    PlaceObj('ModItemCode', {
+        'name', "BetterZoomModScript",
+        'CodeFileName', "Code/BetterZoomModScript.lua"
+    }), 
+    PlaceObj('ModItemOptionChoice', {
+        'name', "BetterZoomModCameraTacMaxZoom", 
+        'comment',
+        "Vanilla Default = 130, Mod Default = 300", 
+        'DisplayName',
+        "Maximum Camera Zoom", 
+        'Help',
+        "Defines how far the camera may zoom out (Vanilla Default = 130)",
+        'DefaultValue', "300", 
+        'ChoiceList', {
+            "130", 
+            "150", 
+            "200", 
+            "250", 
+            "300", 
+            "400", 
+            "500", 
+            "600", 
+            "700", 
+            "800"
+        }
+    }), 
+    PlaceObj('ModItemOptionChoice', {
+        'name', "BetterZoomModCameraTacMinZoom", 
+        'comment', "Vanilla Default = 65, Mod Default = 70", 
+        'DisplayName', "Minimum Camera Zoom", 
+        'Help', "Defines how far the camera may zoom in (Vanilla Default = 65)",
+        'DefaultValue', "70", 
+        'ChoiceList',
+        {
+            "100", 
+            "90", 
+            "80", 
+            "70", 
+            "65", 
+            "60", 
+            "50", 
+            "40", 
+            "30", 
+            "20", 
+            "10"
+        }
+    }), 
+    PlaceObj('ModItemOptionChoice', {
+        'name', "BetterZoomModCameraTacMaxZoomAI", 
+        'comment', "Vanilla Default = 130, Mod Default = 130", 
+        'DisplayName', "Maximum Camera Zoom During AI Turns", 
+        'Help', "Defines the camera's zoom level during enemy turns (Vanilla Default = 130)",
+        'DefaultValue', "130", 
+        'ChoiceList', {
+            "130", 
+            "150", 
+            "200", 
+            "250", 
+            "300", 
+            "400", 
+            "500", 
+            "600", 
+            "700", 
+            "800"}
+    }),
+    PlaceObj('ModItemOptionChoice', {
+        'name', "BetterZoomModCameraTacZoomStep", 
+        'comment', "Vanilla Default = 25, Mod Default = 50", 
+        'DisplayName',"Zoom Step Size", 
+        'Help', "Defines how fast the camera zoom (Vanilla Default = 25)",
+        'DefaultValue', "50", 
+        'ChoiceList',
+        {
+            "5", 
+            "10", 
+            "25", 
+            "50", 
+            "75", 
+            "100", 
+            "125"
+        }
+    })
 }
